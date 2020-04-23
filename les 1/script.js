ca431c71bd1f821697273d302496a4fe33eb2034
Lander Diericx <lander13102000@gmail.com>
2020-04-23 21:16:16 +0200

    les-1

---
+++ les 1/script.js
@@ -0,0 +1,8 @@
+let code = prompt('Lander');
+        console.log(code);
+
+        let divText = 'Je naam is ' + code;
+        document.getElementById('cool-div').innerHTML = divText;
+        
+       // let coolDiv = document.getElementById('cool-div');
+       // coolDiv.innerHTML = divText;
