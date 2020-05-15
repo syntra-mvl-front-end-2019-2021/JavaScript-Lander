94bc28e2dd05df581716cbb095979a0122aa4717
Lander Diericx <lander13102000@gmail.com>
2020-05-15 03:59:24 +0200

    init les 4 oefeningen

---
+++ les 3/loops.js
@@ -0,0 +1,11 @@
+let teller = 1;
+let getal = 10;
+console.log('While loop');
+while(teller <= 10){
+    console.log(getal - teller);
+    teller++;
+}
+console.log('For loop');
+for(let i = 0; i <= 10; i++){
+    console.log(getal - i);
+}
